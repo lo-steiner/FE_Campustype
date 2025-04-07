af385de6f579c7dc0333411f7f7c4fbf8b0fea4d
Oulei <olivier.deszynski@ict-campus.net>
2025-04-07 14:23:23 +0200

    Added Register with validation

---
+++ pages/register.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterForm from "../components/Account/RegisterForm.jsx";
+
+export default function Login({ posts }) {
+    return (
+        <>
+            <RegisterForm/>
+        </>
+    );
+}
