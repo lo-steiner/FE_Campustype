8b1d4534a01154ba04338335aec6145c5b5f6e4c
Levy Schaerer <levy.schaerer@ict-campus.net>
2025-04-08 15:02:46 +0200

    leaderboard bugFix

--- components/Leaderboard/leaderboard.jsx
+++ components/Leaderboard/leaderboard.jsx
@@ -2,8 +2,23 @@ import { useEffect, useState } from "react";
 import LeaderboardAPI from "../../lib/api/Leaderboard.js";
 import styles from './styles.module.css'
 
+const STORAGE_KEY = 'session'
+
 export default function leaderboard() {
     const[ users, setUsers ] = useState([]);
+    const months = [
+        "Januar", "Februar", "März", "April", "Mai", "Juni",
+        "Juli", "August", "September", "Oktober", "November", "Dezember"
+    ];
+
+    const getUsername = () => {
+        const storedData = localStorage.getItem(STORAGE_KEY);
+        if (storedData) {
+            const sessionData = JSON.parse(storedData);
+            return sessionData.username;
+        }
+        return null;
+    };
 
     useEffect(() => {
         const getUsers = async () => {
@@ -14,7 +29,6 @@ export default function leaderboard() {
                 console.error("Failed to load Users: ", error)
             }
         }
-
         getUsers()
     }, [])
 
@@ -22,14 +36,25 @@ export default function leaderboard() {
     
     return (
         <div className={styles.leaderboardContainer}>
-            <h1 className={styles.leaderboardTitle}>LEADERBOARD</h1>
-            {users.length > 0 && (
-                <ul className={styles.leaderboardList}>
-                {users.map((user, i) => (
+          <h1 className={styles.leaderboardTitle}>LEADERBOARD</h1>
+          {users.length > 0 && (
+            <ul className={styles.leaderboardList}>
+              {users.map((user, i) => {
+                const username = getUsername()
+                const timestamp = user.timestamp;
+                const date = new Date(timestamp);
+                const formattedDate = date.toLocaleString("en-US", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                });
+                
+
+                return (
                   <li
                     key={user.id}
                     className={
-                      localStorage.getItem("username") === user.username
+                        username === user.user.username
                         ? styles.currentUser
                         : styles.rangElement
                     }
@@ -39,11 +64,12 @@ export default function leaderboard() {
                     </h2>
                     <h3 className={styles.rangStat}>WPM: {user.wpm}</h3>
                     <h3 className={styles.rangStat}>Accuracy: {user.accuracy}%</h3>
-                    <h3 className={styles.rangStat}>{user.timestamp}</h3>
+                    <h3 className={styles.rangStat} id={styles.rangStat}>{formattedDate}</h3>
                   </li>
-                ))}
-              </ul>
-            )}
+                );
+              })}
+            </ul>
+          )}
         </div>
-    );
+      );
 }
