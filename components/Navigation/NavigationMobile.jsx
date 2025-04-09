8c129b40cd805c85593d0ab22a343333d7ce9a0b
Oulei <olivier.deszynski@ict-campus.net>
2025-04-09 16:54:35 +0200

    Added filtering to leaderboard, differnet writing modes, Navbar Mobile close fix and other things (idk them)

--- components/Navigation/NavigationMobile.jsx
+++ components/Navigation/NavigationMobile.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styles from './NavigationMobile.module.css';
 import { useGlobalContext } from "../../store/index.js";
 
-const NavigationMobile = ({ isOpen }) => {
+const NavigationMobile = ({ isOpen, setIsOpen }) => {
     const { session, login, logout } = useGlobalContext();
     const [isClient, setIsClient] = useState(false);
 
@@ -11,38 +11,52 @@ const NavigationMobile = ({ isOpen }) => {
         setIsClient(true);
     }, []);
 
+    const handleNavPress = () => {
+        setIsOpen(false);
+    };
+
+    const handleLogOut = () => {
+        setIsOpen(false);
+        logout();
+    };
+
     return (
         <div className={`${styles.navTab} ${isOpen ? styles.open : ''}`}>
             <Link href="/" title="HomePage">
-                <div className={`${styles.navTabLinks}`}>
+                <div className={`${styles.navTabLinks}`} onClick={handleNavPress}>
                     <p>Home</p>
                 </div>
             </Link>
+            <Link href="/leaderboard" title="Leaderboard">
+                <div className={styles.navTabLinks} onClick={handleNavPress}>
+                    <p>Leaderboard</p>
+                </div>
+            </Link>
             {session ? (
-                <Link href="/profile" title="Profile">
-                    <div className={styles.navTabLinks}>
-                        <p>Profil</p>
+                <Link href="/#" title="Profile">
+                    <div className={styles.navTabLinks} onClick={handleNavPress}>
+                        <p>Profile</p>
                     </div>
                 </Link>
             ) : (
-                <Link href="/login" title="Profile">
-                    <div className={styles.navTabLinks}>
+                <Link href="/login" title="Login">
+                    <div className={styles.navTabLinks} onClick={handleNavPress}>
                         <p>Login</p>
                     </div>
                 </Link>
             )}
-            <Link href="/posts/create" title="Profile">
-                <div className={styles.navTabLinks}>
-                    <p>Create</p>
-                </div>
-            </Link>
             {session && (
-                <Link href="/" title="Profile" onClick={() => logout()}>
-                    <div className={styles.navTabLinks}>
+                <Link href="/" title="Logout">
+                    <div className={styles.navTabLinks} onClick={handleLogOut}>
                         <p>Logout</p>
                     </div>
                 </Link>
             )}
+            <Link href="/#" title="About Us">
+                <div className={styles.navTabLinks} onClick={handleNavPress}>
+                    <p>About Us</p>
+                </div>
+            </Link>
         </div>
     );
 };
