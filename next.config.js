5d2e33fc2ecad425cbf393f77d2bbc94a04e883d
Oulei <olivier.deszynski@ict-campus.net>
2025-04-18 14:09:55 +0200

    Configure Next.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/<your-repo-name>' : '',
+  assetPrefix: isProd ? '/<your-repo-name>/' : '',
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
