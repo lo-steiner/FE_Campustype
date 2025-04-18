73d8d34fe9512b281361911e418a3f50086fdae9
Oulei <olivier.deszynski@ict-campus.net>
2025-04-18 14:16:57 +0200

    Final addition for github pages

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -6,13 +6,13 @@ export default function AboutUs() {
     <div className={styles.body}>
         <div className={styles.imgContainer}>
             <div className={styles.images}>
-                <Link href="/profile/161"><img id={styles.levy} src="/Levy.png" alt="Bildbeschreibung"/></Link>
+                <Link href="/profile/161"><img id={styles.levy} src="/Levy.png" alt="Levy"/></Link>
                 <h1>Levy</h1>
                 <h3>Italian</h3>
             </div>
             
             <div className={styles.images}>
-                <Link href="/profile/132"><img id={styles.oli} src="/Oli.png" alt="Bildbeschreibung"/></Link>
+                <Link href="/profile/132"><img id={styles.oli} src="/Oli.png" alt="Oli"/></Link>
                 <h1>Oli</h1>
                 <h3>K*rwa Bober</h3>
             </div>
